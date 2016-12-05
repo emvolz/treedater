91d7492335c84b3e31f45f45d1c4f049afd346ba
emvolz <erik@erikvolz.info>
2016-12-05 15:03:21 +0000

    Added 2 options: strictClock switches to Poisson model (similar to LF). temporalConstraints uses constrained least squares (package mgcv) to optimise Ti such that daughters always occur after ancestors (similar to QPD in lsd).

--- R/lsd1.R
+++ R/lsd1.R
@@ -8,7 +8,7 @@
 
 
 require(ape)
-
+require(mgcv)
 
 .make.tree.data <- function( tre, sampleTimes, s, cc)
 {
@@ -38,13 +38,24 @@ require(ape)
 	A[ cbind(1:nrow(tre$edge), tre$edge[,1]-n) ] <- -1
 	internalEdges <- setdiff( 1:length(B), tipEdges )
 	A[ cbind(internalEdges, tre$edge[internalEdges,2] - n) ] <- 1
+	
+	# constraints(optional)
+	Ain <-  matrix(0, nrow = length(B), ncol = n-1)
+	Ain[ cbind(1:nrow(tre$edge), tre$edge[,1]-n) ] <- 1 # parent to -1
+	Ain[ cbind(internalEdges, tre$edge[internalEdges,2] - n) ] <- -1 # dgtr to +1
+	bin <- rep(0, length(B))
+	bin[tipEdges] <- sts # terminal edges to -sample time #...
+	
 	W <- abs( 1/((tre$edge.length + cc / s)/s) ) 
 	list( A0 = A, B0 = B, W0 = W, n = n,  tipEdges=tipEdges
 	 , i_tip_edge2label = i_tip_edge2label
 	 , sts2 = sts 
 	 , s = s, cc = cc, tre = tre
 	 , daughters = daughters
-	 , parent = parent)
+	 , parent = parent
+	 , Ain = Ain
+	 , bin = bin
+	)
 }
 
 .Ti2blen <- function(Ti, td ){
@@ -82,14 +93,14 @@ require(ape)
 		A <- omegas * td$A0 
 		B <- td$B0
 		B[td$tipEdges] <- td$B0[td$tipEdges] -  unname( omegas[td$tipEdges] * td$sts2 )
-		#TODO scale var by delta T? 
 		if (scale_var_by_rate){
 			return( coef( lm ( B ~ A -1 , weights = td$W/omegas) ) )
 		} else{
 			return( coef( lm ( B ~ A -1 , weights = td$W) ) ) 
 		}
 }
-# TODO could also try nls to estimate log(T_i) OR package limSolve to add constraints T_i > 0	
+
+# TODO could also try  package limSolve to add constraints T_i > 0	
 .optim.Ti1 <- function( omegas, Ti, td ){
 	blen <- .Ti2blen( Ti, td )
 		A <- omegas * td$s *  td$A0 
@@ -105,6 +116,32 @@ require(ape)
 	B[td$tipEdges] <- td$B0[td$tipEdges] -  unname( omegas[td$tipEdges] * td$sts2 )
 	B <- B * td$s
 	sum( dnorm( (B - A %*% Ti ), mean=0 , sd=sqrt((td$s*blen*omegas))  , log = T) )
+}
+# constraints using quad prog 
+.optim.Ti2 <- function( omegas, td ){
+		A <- omegas * td$A0 
+		B <- td$B0
+		B[td$tipEdges] <- td$B0[td$tipEdges] -  unname( omegas[td$tipEdges] * td$sts2 )
+		
+		# initial feasible parameter values:
+		p0 <- ( coef( lm ( B ~ A -1 , weights = td$W/omegas) ) )
+		p1 <- .hack.times1(p0, td )
+		
+		# design
+		M <- list( 
+			X  = A
+			,p = p1
+			,off = c()# rep(0, np)
+			,S=list()
+			,Ain=-td$Ain
+			,bin=-td$bin
+			,C=matrix(0,0,0)
+			,sp= c()#rep(0,np)
+			,y=B
+			,w=td$W/omegas
+		)
+		o <- pcls(M)
+	o
 }
 
 .optim.omegas.gammaPoisson0 <- function( Ti, r, gammatheta, td )
@@ -152,35 +189,30 @@ require(ape)
 	sum( omegas * blen ) / sum(blen)
 }
 
-.hack.times <- function(Ti, td)
+.hack.times1 <- function(Ti, td)
 {
-	tre <- td$tre
-	t <- c( sts[td$tre$tip.label], Ti)
+	t <- c( td$sts2[td$tre$tip.label], Ti)
+	inodes <- (td$n+1):length(t)
 	
-	blen <- .Ti2blen( Ti, td )
-	
-	done <- FALSE
-	while(!done)
+	repeat
 	{
-		for(a in (td$n+1):length(t)){
-			done <- TRUE
-			if ( any( t[a] > t[td$daughters[a,]] ) ){
-				done <- FALSE
-				t[a] <- min( t[td$daughters[a,]] , na.rm=T) + td$minblen
-			}
-		}		
+		.t <- t
+		.t[inodes] <- pmin(t[inodes], -td$minblen + pmin( t[ td$daughters[inodes,1] ], t[td$daughters[inodes,2] ] ) )
+		if (identical( t, .t )) break
+		t <- .t
 	}
-	ov <- t[(td$n+1):length(t)]
-	ov
+	t[inodes]
 }
 
 treedater = dater <- function(tre, sts, s=1e3
  , omega0 = NA
  , minblen = NA
- , maxit=30
+ , maxit=100
  , abstol = .01
- , searchRoot = 3
+ , searchRoot = 5
  , quiet = FALSE
+ , temporalConstraints = TRUE
+ , strictClock = FALSE
 )
 { 
 	THETA_LB <- 1e-3
@@ -194,10 +226,10 @@ treedater = dater <- function(tre, sts, s=1e3
 		if (!quiet) cat( 'Tree is not rooted. Searching for best root position. Increase searchRoot to try harder.\n')
 		searchRoot <- round(searchRoot )
 		rtres <- .multi.rtt(tre, sts, topx=searchRoot)
-		tds <- lapply( rtres, function(t) dater( t, sts, s = s, omega0=omega0, minblen=minblen, maxit=maxit,abstol=abstol) )
+		tds <- lapply( rtres, function(t) dater( t, sts, s = s, omega0=omega0, minblen=minblen, maxit=maxit,abstol=abstol
+		 , strictClock = strictClock, temporalConstraints = temporalConstraints, quiet = quiet) )
 		lls <- sapply( tds, function(td) td$loglik )
 		return ( tds [[ which.max( lls ) ]] )
-		#tre <- rtt( tre , sts)
 	} else{
 		if (!quiet) cat( 'Tree is rooted. Not estimating root position.\n')
 	}
@@ -217,11 +249,10 @@ treedater = dater <- function(tre, sts, s=1e3
 	}
 	td <- .make.tree.data(tre, sts, s, cc )
 	td$minblen <- minblen
-	#attach(td)
 	
 	# initial gamma parms with small variance
-	gammatheta0 = gammatheta <- 1e-2
-	r0 = r <- omega0 * td$s / gammatheta0
+	r0 = r <- ifelse( strictClock, omega0 , omega0 * td$s / 1e-2)
+	gammatheta0 = gammatheta <- ifelse( strictClock, 0,  1e-2 )
 	
 	done <- FALSE
 	lastll <- -Inf
@@ -230,13 +261,11 @@ treedater = dater <- function(tre, sts, s=1e3
 	omegas <- rep( omega0,  nEdges )
 	.trace <- list()
 	while(!done){
-		#if (iter == 0){
-		#	Ti <- .optim.Ti0( omegas, td, scale_var_by_rate )
-		#} else{
-		#	Ti <- .optim.Ti1( omegas, Ti, td )
-		#}
-		Ti <- .optim.Ti0( omegas, td, scale_var_by_rate )
-		#Ti <- .hack.times( Ti, td ) # TODO unclear if this helps
+		if (temporalConstraints){
+			Ti <- .optim.Ti2( omegas, td)
+		} else{
+			Ti <- .optim.Ti0( omegas, td, scale_var_by_rate )
+		}
 		
 		if (gammatheta < THETA_LB){
 			# switch to poisson model
@@ -253,8 +282,6 @@ treedater = dater <- function(tre, sts, s=1e3
 			gammatheta <- o$gammatheta
 			omegas <- .optim.omegas.gammaPoisson0( Ti, o$r, o$gammatheta, td ) 
 		}
-		#i <- (iter %% length(omegas))+1
-		#omegas[i] <- .optim.omega_i.gammaPoisson0( Ti, o$r, o$gammatheta, td, i ) 
 		
 		ti_ll <- .Ti.ll1( omegas, Ti, td ) #
 		ll <- ll + ti_ll
