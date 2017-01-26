1f653eda0f3904f013ad335338f08febaa676483
emvolz <erik@erikvolz.info>
2017-01-26 18:48:40 +0000

    CI based on sd of replicates; defaults to temporal unconstrained optim to improve speed

--- R/parboot.R
+++ R/parboot.R
@@ -1,5 +1,5 @@
 
-parboot.treedater <- function( td , nreps = 100, level = .95 )
+parboot.treedater <- function( td , nreps = 100, level = .95, overrideTempConstraint=T )
 {
 	alpha <- min(1, max(0, 1 - level ))
 	lapply( 1:nreps, function(k) 
@@ -20,11 +20,13 @@ parboot.treedater <- function( td , nreps = 100, level = .95 )
 		if (!td$intree_rooted) tre <- unroot( tre )
 		est <- NULL
 		if (td$EST_SAMP_TIMES) est <- td$estimateSampleTimes
+		tempConstraint <- td$temporalConstraints
+		if ( overrideTempConstraint) tempConstraint <- FALSE
 		td <- dater(tre, td$sts
 		 , omega0 = td$meanRate
 		 , minblen = td$minblen
 		 , quiet = TRUE
-		 , temporalConstraints = td$temporalConstraints
+		 , temporalConstraints = tempConstraint
 		 , strictClock = ifelse( td$clock=='strict' , TRUE, FALSE )
 		 , estimateSampleTimes = est
 		 , estimateSampleTimes_densities = td$estimateSampleTimes_densities
@@ -39,12 +41,16 @@ parboot.treedater <- function( td , nreps = 100, level = .95 )
 	meanRates <- sapply( tds, function(td) td$meanRate )
 	cvs <- sapply( tds, function(td) td$coef_of_variation )
 	tmrcas <- sapply( tds, function(td) td$timeOfMRCA )
+	mr_sd <- sd(meanRates)
+	tmrca_sd <- sd( tmrcas )
 	rv <- list( 
 		trees = tds
 		, meanRates = meanRates
-		, meanRate_CI = quantile( meanRates, probs = c(alpha/2, 1-alpha/2 ))
+		, meanRate_CI = c( max(0, td$meanRate - mr_sd*1.96), td$meanRate + mr_sd*1.96 )
+		#, meanRate_CI = quantile( meanRates, probs = c(alpha/2, 1-alpha/2 ))
 		, coef_of_variation_CI = quantile( cvs, probs = c(alpha/2, 1 - alpha/2))
-		, timeOfMRCA_CI = quantile( tmrcas, probs = c(alpha/2, 1 - alpha/2))
+		, timeOfMRCA_CI = c( td$timeOfMRCA - tmrca_sd*1.96, td$timeOfMRCA + tmrca_sd*1.96 )
+		#, timeOfMRCA_CI = quantile( tmrcas, probs = c(alpha/2, 1 - alpha/2))
 		, td = td
 		, alpha = alpha
 		, level = level
