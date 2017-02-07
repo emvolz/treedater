40054b2c904bcfa8e4ff55a6fd823d5cde2b28e2
emvolz <erik@erikvolz.info>
2017-02-07 11:49:42 +0000

    outlier.tips function reports q values (adjusted p values for false discovery rate); will only work if temporalConstrains=TRUE

--- R/outliers0.R
+++ R/outliers0.R
@@ -1,5 +1,8 @@
 
-outlier.tips <- function(td, alpha = .05){
+outlier.tips <- function(td, alpha = .01){
+	if ( !td$temporalConstraints ){
+		stop('The outlier.tips function requires a treedater object fitted using temporalConstraints=TRUE. Quitting.')
+	}
 	if (td$clock=='relaxed'){
 		with(td,{
 			blen <- pmax( minblen, tre$edge.length )
@@ -26,18 +29,20 @@ outlier.tips <- function(td, alpha = .05){
 		}) -> p
 	}
 	p[ p > .5] <- 1 - p[ p > .5]
+	p <- p * 2 
 	n <- length( td$tre$tip.label )
 	tipEdges <- which( td$tre$edge[,2] <= n )
 	qu.df <- data.frame( taxon = td$tre$tip.label[ td$tre$edge[tipEdges,2]]
-	  , loglik = unname( lls[tipEdges])
+	  , q = unname( p.adjust( p[tipEdges], method = 'fdr' ) )
 	  , p  = unname( p[tipEdges] ) 
+	  , loglik = unname( lls[tipEdges])
 	  , rates = unname( td$omegas[tipEdges] )
 	  , branch.length = td$tre$edge.length[ tipEdges] 
 	)
-	qu.df <- qu.df[ order( qu.df$loglik), ]
+	qu.df <- qu.df[ order( qu.df$q), ]
 	rownames(qu.df) <- qu.df$taxon
 	qu.df <- qu.df[,-1] 
-	qu.df1 <- qu.df[ qu.df$p <  alpha , ]
+	qu.df1 <- qu.df[ qu.df$q <  alpha , ]
 	
 	print( qu.df1 )
 	qu.df
