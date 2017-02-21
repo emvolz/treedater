6b112ee00bcc557c9d5310716f5a2cfa6f09794a
emvolz <erik@erikvolz.info>
2017-02-21 11:29:47 +0000

    nonparametric bootstrap

--- R/parboot.R
+++ R/parboot.R
@@ -101,11 +101,11 @@ parboot.treedater <- function( td , nreps = 100,  overrideTempConstraint=T, over
 		, tmrcas = tmrcas
 		, ttmrcas = ttmrcas
 	)
-	class(rv) <- 'parboot.treedater'
+	class(rv) <- 'boot.treedater'
 	rv
 }
 
-print.parboot.treedater <- function( x, ... )
+print.parboot.treedater = print.boot.treedater <- function( x, ... )
 {
 rns <- c( 'Time of common ancestor' 
   , 'Mean substitution rate'
@@ -125,7 +125,7 @@ o <- data.frame( pseudoML= c(x$td$timeOfMRCA, x$td$meanRate)
 	invisible(x)
 }
 
-plot.parboot.ltt <- function(pbtd, t0 = NA, res = 100, ... )
+plot.parboot.ltt = plot.boot.ltt <- function(pbtd, t0 = NA, res = 100, ... )
 {
 	t1 <- max( pbtd$td$sts, na.rm=T )
 	if (is.na(t0)) t0 <- min( sapply( pbtd$trees, function(tr) tr$timeOf ) )
@@ -169,4 +169,99 @@ relaxed.clock.test <- function( ..., nreps=100, overrideTempConstraint=T )
 	 , parboot_strict = pbtd
 	 , nullHypothesis_coef_of_variation_CI = cvci_null
 	)
+}
+
+
+##
+boot.treedater <- function( td, tres, overrideTempConstraint=TRUE, searchRoot=1 , overrideClock=NULL, nreps = 100, quiet=TRUE, normalApproxTMRCA=F, ncpu = 1, parallel_foreach = FALSE )
+{
+	if (quiet){
+		cat( 'Running in quiet mode. To print progress, set quiet=FALSE.\n')
+	}
+	if (overrideTempConstraint){
+		cat('NOTE: Running with overrideTempConstraint will speed up execution but may underestimate variance.\n')
+	}
+	if (length(tres) < nreps){
+		warning('Number of non-parametric bootstrap trees is less than number of bootstrap replicates requested. Results will under-estimate variance.')
+	}
+	level <- .95
+	alpha <- min(1, max(0, 1 - level ))
+
+	.boot.replicate <- function(  )
+	{
+		k <- sample.int( length(tres), size=1)
+		tre <- tres[[k]]
+		
+		est <- NULL
+		if (td$EST_SAMP_TIMES) est <- td$estimateSampleTimes
+		tempConstraint <- td$temporalConstraints
+		if ( overrideTempConstraint) tempConstraint <- FALSE
+		clockstr <- td$clock
+		if (!is.null( overrideClock)){
+			if (is.na(overrideClock)) stop('overrideClock NA. Quitting.')
+			if (!overrideClock %in% c('strict', 'relaxed') ){
+				stop('overrideClock must be one of strict or relaxed')
+			}
+			clockstr <- overrideClock
+		}
+		strictClock <- ifelse( clockstr=='strict' , TRUE, FALSE )
+		td2 <- dater(tre, td$sts
+		 , omega0 = NA
+		 , minblen = td$minblen
+		 , quiet = TRUE
+		 , searchRoot = searchRoot
+		 , temporalConstraints = tempConstraint
+		 , strictClock = strictClock
+		 , estimateSampleTimes = est
+		 , estimateSampleTimes_densities = td$estimateSampleTimes_densities
+		 , numStartConditions = td$numStartConditions 
+		)
+		if (suppressWarnings( is.na( td2[1])) ) return (NA )
+			
+		if (!quiet){
+			cat('\n #############################\n')
+			cat( paste( '\n Replicate', k, 'complete \n' ))
+			print( td2 )
+		}
+		td2
+	}
+
+	if (ncpu > 1)
+	{
+		if (parallel_foreach){
+			tds <- foreach( k = 1:nreps, .packages=c('treedater') ) %dopar% .boot.replicate()
+		} else{
+			tds <- parallel::mclapply( 1:nreps, function(k) .boot.replicate() 
+			, mc.cores = ncpu ) 
+		}
+	} else{
+		tds <- foreach( k = 1:nreps, .packages=c('treedater') ) %do% .boot.replicate( )
+	}
+	tds <- tds[!suppressWarnings ( sapply(tds, function(td) is.na(td[1]) ) )  ] 
+	# output rate CIs, parameter CIs, trees
+	meanRates <- sapply( tds, function(td) td$meanRate )
+	cvs <- sapply( tds, function(td) td$coef_of_variation )
+	tmrcas <- sapply( tds, function(td) td$timeOfMRCA )
+	ttmrcas <- sapply( tds, function(td) td$timeTo )
+	log_mr_sd <- sd(log(meanRates))
+	log_tmrca_sd <- sd( log(ttmrcas ) )
+	if (normalApproxTMRCA){
+		timeOfMRCA_CI <- c( td$timeOfMRCA * exp(-log_tmrca_sd*1.96), td$timeOfMRCA * exp(log_tmrca_sd*1.96 ))
+	} else {
+		timeOfMRCA_CI <- quantile( tmrcas, p = c(.025, .975 ))
+	}
+	rv <- list( 
+		trees = tds
+		, meanRates = meanRates
+		, meanRate_CI = c( exp( log(td$meanRate) - log_mr_sd*1.96), exp(log(td$meanRate) + log_mr_sd*1.96 ))
+		, coef_of_variation_CI = quantile( cvs, probs = c(alpha/2, 1 - alpha/2))
+		, timeOfMRCA_CI = timeOfMRCA_CI
+		, td = td
+		, alpha = alpha
+		, level = level
+		, tmrcas = tmrcas
+		, ttmrcas = ttmrcas
+	)
+	class(rv) <- 'boot.treedater'
+	rv
 }
