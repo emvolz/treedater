3b87a790f500fe6535bbf8f418e790f00e59a2fc
emvolz <erik.volz@gmail.com>
2018-04-23 17:16:32 +0100

    refactor + docs

---
+++ R/treedater.R
@@ -0,0 +1,7 @@
+#' treedater fits a strict or relaxed molecular clock to a phylogenetic tree and estimates evolutionary rates and times of common ancestry. The calendar time of each sample must be specified (possibly with bounds of uncertainty) and the length of the sequences used to estimate the tree.
+#'
+#' treedater uses heuristic search to optimise the TMRCAs of a phylogeny and the substitution rate. An uncorrelated relaxed molecular clock accounts for rate variation between lineages of the phylogeny which is parameterised using a Gamma-Poisson mixture model.
+#' @references
+#' Volz, E. M., and S. D. W. Frost. "Scalable relaxed clock phylogenetic dating." Virus Evolution 3.2 (2017).
+"_PACKAGE"
+#> [1] "_PACKAGE"
